a542633ec3a724cc524da6539e8f93ab696c92b8
Leed Almogbel <leedalmogbel@gmail.com>
2023-11-08 04:36:56 +0800

    update on Slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -23,7 +23,46 @@ const Arrow = styled.div`
   right: ${props => props.direction === 'right' && '10px'};
   bottom: 0;
   margin:auto;
+  cursor: pointer;
+  opacity: 0.5;
 `;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button``;
+
 
 const Slider = () => {
   return (
@@ -31,6 +70,18 @@ const Slider = () => {
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU"></Image>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>Winter Sale</Title>
+            <Desc>Dont Compromise on Style! get flat 30% off now for new arrivals.</Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
