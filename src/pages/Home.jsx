e17a3c9676218b5d24d51eac1dfef6e66f5a57bc
Leed Almogbel <leedalmogbel@gmail.com>
2023-11-07 21:20:09 +0800

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Announcements from "../components/Announcements";
+import Navbar from "../components/NavBar";
+import Slider from "../components/Slider";
+
+const Home = () => {
+  return (
+    <div>
+        <Announcements />
+        <Navbar />
+        <Slider />
+    </div>
+  )
+}
+
+export default Home;
