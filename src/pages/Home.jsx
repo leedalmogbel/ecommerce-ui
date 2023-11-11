de91d5025acfdb19abf2ab5274ab285b6b2daf6a
Leed Almogbel <leedalmogbel@gmail.com>
2023-11-11 22:45:53 +0800

    products intial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Announcements from "../components/Announcements";
 import Categories from "../components/Categories";
 import Navbar from "../components/NavBar";
 import Slider from "../components/Slider";
+import Products from "../components/Products";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <Navbar />
         <Slider />
         <Categories />
+        <Products />
     </div>
   )
 }
